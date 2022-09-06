15409e29052989152e38c472a4290f283aeb0b04
KGE TECHNOLOGIES PVT LTD <kgetechnologies@gmail.com>
2022-09-07 03:54:57 +0530

    SEO Fixes

--- PublicWebsite/Controllers/ProductController.cs
+++ PublicWebsite/Controllers/ProductController.cs
@@ -11,12 +11,12 @@ namespace cheapdscin.Controllers
         }
         public ActionResult CheapClass3Dsc()
         {
-            ViewBag.DisplayName = "Credit Card to Cash Support & Services";
+            ViewBag.DisplayName = "Cheap Class 3 DSC";
             ViewBag.LinkValue = "";
 
-            ViewBag.CanonicalUri = "Services on ";
-            ViewBag.desc = "Services on  Instant Cash on Credit Card | Services on  Spot Cash on Credit Card | Services on  Credit Card to Cash | Services on  Cash on Credit card";
-            ViewBag.Title = "Services on  Instant Cash | Services on  Spot Cash | Services on  Credit Card to Cash | Services on  Cash on Credit Card";
+            ViewBag.CanonicalUri = "product/CheapClass3Dsc";
+            ViewBag.desc = "Class 3 DSC, Cheap Class 3 DSC, Cheap Class 3, Class 3 Digital Signature Certificate";
+            ViewBag.Title = "Class 3 DSC, Cheap Class 3 DSC, Cheap Class 3, Class 3 Digital Signature Certificate";
 
 
 
@@ -25,23 +25,23 @@ namespace cheapdscin.Controllers
 
         public ActionResult CheapDgft()
         {
-            ViewBag.DisplayName = "Instant Cash on Credit Card";
+            ViewBag.DisplayName = "Cheap DGFT Certificate";
             ViewBag.LinkValue = "";
 
-            ViewBag.CanonicalUri = "/services/InstantCashOnCreditCard";
-            ViewBag.desc = "Instant Cash on Credit Card | Instant Cash on Card | Credit Card Instant Cash | Card to Instant Cash";
-            ViewBag.Title = "Instant Cash on Credit Card | Instant Cash on Card | Credit Card Instant Cash | Card to Instant Cash";
+            ViewBag.CanonicalUri = "product/CheapDgft";
+            ViewBag.desc = "Cheap DGFT Certificate, DFGT Certificate, Cheap DGFT";
+            ViewBag.Title = "Cheap DGFT Certificate, DFGT Certificate, Cheap DGFT";
 
             return View("CheapDgft");
         }
         public ActionResult CheapUsbToken()
         {
-            ViewBag.DisplayName = "Spot Cash on Credit Card";
+            ViewBag.DisplayName = "Cheap USB Token";
             ViewBag.LinkValue = "";
 
-            ViewBag.CanonicalUri = "/services/InstantCashOnCreditCard";
-            ViewBag.desc = "Spot Cash on Credit Card | Spot Cash on Card | Credit Card Spot Cash | Card to Spot Cash";
-            ViewBag.Title = "Spot Cash on Credit Card | Spot Cash on Card | Credit Card Spot Cash | Card to Spot Cash";
+            ViewBag.CanonicalUri = "/product/CheapUsbToken";
+            ViewBag.desc = "Cheap Usb Token, Usb Token, Cheap Dsc Usb Token, DSC Usb Token";
+            ViewBag.Title = "Cheap Usb Token, Usb Token, Cheap Dsc Usb Token, DSC Usb Token";
             
             return View("CheapUsbToken");
         }
